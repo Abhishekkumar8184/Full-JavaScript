bf21e6c6360524b6ac395bb1e529587e32217194
Abhishek Kumar <abhishekaryan8184@gmail.com>
2024-12-04 01:55:18 +0000

    data type

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,41 @@
+"use strict"; // treat all JS code as newer View
+
+ // alert(2+2) // we are using nodejs ,not browser
+
+  //console.log(30
+ 
+ // +30); // console.log("abhishek"); // code readability should be high
+
+
+  //offial JS Ducomention is https://tc39.es/ecma262
+
+
+ 
+let name = "Abhishek"
+let age = 10
+let isLoggedId = false
+
+//   Number=> 2 to power 53 ( range)
+
+//   bigint (trading ,big website)
+
+//   string =>  ""
+
+//  Boolean => true /false
+
+//  null => standanone value
+
+//   undifined => 
+
+    // symbol => unique
+
+// object 
+
+//console.log(typeof"age");
+ // console.log(typeof age);
+
+
+
+//console.log(typeof undefined);   //undefined
+//console.log(typeof null);//  objects
+
