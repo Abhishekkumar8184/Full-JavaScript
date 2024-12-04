a0d7e10ef9337cbaeadb81675e144f4510a0f19a
Abhishek Kumar <abhishekaryan8184@gmail.com>
2024-12-04 04:32:52 +0000

    conversion& operation

---
+++ 01_basics/03_conversion&Operation.js
@@ -0,0 +1,73 @@
+let score="33abc"
+
+//    console.log(typeof score);
+//    console.log(typeof (score));
+
+let valuveInNumber=Number(score)               // Number is use for conversion string to number
+//     console.log(typeof valuveInNumber);
+//     console.log(valuveInNumber);
+
+// "33" => 33
+// "33abc" => NaN(not an number)
+// true => 1; false => 0
+ 
+let isLoggedIn = "abhishek"
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+//    console.log(booleanLoggedIn);
+
+// 1 =>true; 0 => false
+//"Abhishek" => true
+// ""> false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+//console.log(stringNumber )
+///console.log(typeof stringNumber )
+
+
+
+//                                         *******************Operations*****************************
+
+let value = 3
+let negVale = -value
+//     console.log(negVale);
+
+//     console.log(3+3);
+//     console.log(3-3);
+//     console.log(3*3);
+//     console.log(3**2);
+//      console.log(2/3);
+ 
+let str1 = "Abhishek"
+let str2 ="Kumar "
+
+let str3 = str1+str2
+//     console.log(str3);
+
+
+//      console.log( "1"+2); 
+
+//       console.log(1+"2");
+
+//       console.log("1"+2+2);                 // if frist is  string  then it behave like string
+
+//      console.log(1+2+"2");                // if frist is  Number  then it behave like Number
+ 
+//        console.log(+true)
+
+//        console.log(true+);
+
+//        console.log(+"");
+ 
+//          https://tc39.es/ecma262/multipage/abstract-operations.html#sec-abstract-operations
+
+let num1,num2,num3 
+num1=num2=num3 =   2+2
+
+let gameCounter = 100
+ ++gameCounter
+ console.log(gameCounter);
+
+ //               https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+ 
