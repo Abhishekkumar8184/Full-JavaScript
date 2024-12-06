7eebbab91594755c09e9babda5873b3abadeff28
Abhishek Kumar <abhishekaryan8184@gmail.com>
2024-12-06 01:02:31 +0000

    objects

---
+++ 02_basics/03_object.js
@@ -0,0 +1,24 @@
+// singleton
+// Object.create
+
+
+
+// object literals
+
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Abhishek ",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "abhishek@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+
+    
+ console.log(JsUser.name)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
