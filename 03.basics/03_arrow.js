7232336c26343fb67592e3b7c3c81a017019bf79
Abhishek Kumar <abhishekaryan8184@gmail.com>
2024-12-08 02:17:15 +0000

    arrow

---
+++ 03.basics/03_arrow.js
@@ -0,0 +1,65 @@
+  
+  
+  
+  const user = {
+    username: "Abhishek",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+//user.welcomeMessage()
+//user.username = "sam"
+//user.welcomeMessage()
+
+// user.welcomeMessage()
+
+ // console.log(this);
+
+//  function chai(){
+//     let username = "Abhishek"
+//     console.log(this.username) } 
+//     chai()
+
+
+//   const chai = function () {
+ //           let username = "hitesh"
+ //           console.log(this.username);
+ //       }
+//    chai()  
+
+//   const chai =  () => {
+ //      let username = "hitesh"
+//       console.log(this);
+//   }
+
+
+//chai()
+
+//   ()=>{ }                   //  arrow fuction
+//  
+//  const addTwo = (num1, num2) => {
+//           return num1 + num2
+//  }
+//  console.log(addTwo(4,9));
+
+//  const addTwo = (num1, num2) =>  num1 + num2       // implicit
+//  console.log(addTwo(4,9));
+
+
+// const addTwo = (num1, num2) =>  (num1 + num2  )     // retun key word need
+
+//const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+ //  console.log(addTwo(3, 4))
+
+ const myArray = [2, 5, 3, 7, 8]
+
+myArray.forEach()
+
+
+
